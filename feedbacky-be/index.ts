a8d67fb25f8c02498f6d3ca8cf4f1578f025dace
EminFurkan <emintezeren@gmail.com>
2023-07-22 23:18:38 +0300

    fix: trying vercel config

--- feedbacky-be/index.ts
+++ feedbacky-be/index.ts
@@ -31,7 +31,7 @@ connectToDB()
   });
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
