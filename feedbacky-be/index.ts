6625ffb49a558a4010f5c2e6a6bfbf557fe03926
EminFurkan <emintezeren@gmail.com>
2023-07-22 23:33:41 +0300

    fix: static content

--- feedbacky-be/index.ts
+++ feedbacky-be/index.ts
@@ -32,7 +32,7 @@ connectToDB()
   });
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname + '/public')));
+app.use("/", express.static(path.join(__dirname + '/public')));
 
 app.set('view engine', 'ejs');
 
