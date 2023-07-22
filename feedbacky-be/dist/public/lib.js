54f5426d7d608c9c0c029423134713a7d1187e52
EminFurkan <emintezeren@gmail.com>
2023-07-22 22:40:06 +0300

    fix: vercel.config

--- feedbacky-be/dist/public/lib.js
+++ feedbacky-be/dist/public/lib.js
@@ -1,18 +1,14 @@
+"use strict";
 const feedbackyContainer = document.createElement('div');
 feedbackyContainer.id = 'root';
-
 document.body.appendChild(feedbackyContainer);
-
 const feedbackyScript = document.createElement('script');
 feedbackyScript.src = 'https://feedbacky-fe-eminfurkan.vercel.app/assets/index-2af8f350.js';
 feedbackyScript.async = true;
 feedbackyScript.crossOrigin = 'anonymous';
 feedbackyScript.type = 'module';
-
 const feedbackyStylesheet = document.createElement('link');
 feedbackyStylesheet.rel = 'stylesheet';
 feedbackyStylesheet.href = 'https://feedbacky-fe-eminfurkan.vercel.app/assets/index-317bc049.css';
-
 document.head.appendChild(feedbackyStylesheet);
-
 document.body.appendChild(feedbackyScript);
